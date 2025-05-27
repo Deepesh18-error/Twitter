e7de79f25f50bec13afa2ad8f516b56cea4e8dd8
DEEPESH SAXENA <deepeshsaxena18@gmail.com>
2025-05-27 12:03:43 +0530

    Added controller and routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import express from 'express'; 
+import bodyParser from 'body-parser';
 
 import { connect } from './config/database.js';
 
 import { HashtagRepository, TweetRepository } from './repository/index.js';
 import service from './services/tweet-service.js';
 
+import apiRoutes from './routes/index.js';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
 
 app.listen(3000, async () => {
   console.log('Server started on port 3000');
@@ -41,11 +48,13 @@ app.listen(3000, async () => {
   // })
   // console.log(tweet);
 
-  let ser = new service();
-  const tweet = await ser.create({
-    content: 'my #coDE and #ComPILER',
-  });
-  console.log(tweet);
+  // let ser = new service();
+  // const tweet = await ser.create({
+  //   content: 'my #coDE and #ComPILER',
+  // });
+  // console.log(tweet);
+
+
 });
  
 
