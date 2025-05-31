87e54cdfed127b8569f2a8a7873689107a9956d5
DEEPESH SAXENA <deepeshsaxena18@gmail.com>
2025-05-31 18:41:13 +0530

    Added login API and tweet from authenticate token

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,9 +15,8 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-    },
-
-}, { timestamps: true });
+    }
+}, {timestamps: true});
 
 userSchema.pre('save', function (next) {
     const user = this;
@@ -26,6 +26,16 @@ userSchema.pre('save', function (next) {
     next();
 });
 
-const User = mongoose.model('User' , userSchema);
+userSchema.methods.comparePassword = function compare(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+userSchema.methods.genJWT = function generate() {
+    return jwt.sign({id: this._id, email: this.email}, 'twitter_secret', {
+        expiresIn: '1h'
+    });
+}
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -5,7 +5,17 @@ class UserRepository extends CurdRepository {
     constructor() {
         super(User);
     }
+
+        async findBy(data) {
+        try {
+            const response = await User.findOne(data);
+            return response;
+        } catch(error) {
+            throw error;
+        }
+    }
 }
+
 
 
 export default UserRepository;
